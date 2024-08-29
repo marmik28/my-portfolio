2cdccced007fc1114f0cfcd399244a9ed82449b6
marmik28 <marmik28112002@gmail.com>
2024-08-29 15:31:46 -0400

    hero page changes

--- Components/header.tsx
+++ Components/header.tsx
@@ -1,19 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 import "../app/globals.css";
 
 const Header: React.FC = () => {
+    const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+    const toggleMobileMenu = () => {
+        setMobileMenuOpen(!isMobileMenuOpen);
+    };
+
+    useEffect(() => {
+        // Disable scrolling when mobile menu is open
+        if (isMobileMenuOpen) {
+            document.body.style.overflow = "hidden";
+        } else {
+            document.body.style.overflow = "auto";
+        }
+    }, [isMobileMenuOpen]);
+
     return (
-        <div className="flex justify-between items-center fixed z-50 top-0 left-0 right-0 mx-auto max-w-screen-xl w-full py-8">
-            <p className="m-0 p-0 font-poppins font-medium text-4xl slide delay">
+        <div className="block">
+        <div className="flex justify-between items-center fixed z-50 top-0 left-0 right-0 mx-auto max-w-screen-xl w-full py-4 bg-white md:py-8">
+            <p className="m-0 py-0 px-4 font-poppins font-medium slide delay text-3xl md:text-4xl">
                 <span className="heroText2">&lt;</span>
-                Marmik Patel /
+                Marmik
                 <span className="heroText2">&gt;</span>
             </p>
-            <div className="flex justify-between text-2xl font-medium">
+            <div className="hidden md:flex justify-between text-2xl font-medium">
                 <div className="px-8 slide delay">Projects</div>
                 <div className="px-8 slide delay">About</div>
                 <div className="px-8 slide delay">Resume</div>
             </div>
+            <div className="md:hidden px-4 slide delay">
+                <button onClick={toggleMobileMenu} className="text-3xl">
+                    {isMobileMenuOpen ? <FaTimes /> : <FaBars />}
+                </button>
+            </div>
+            {isMobileMenuOpen && (
+                <div className="fixed top-20 left-0 right-0 bottom-0 bg-white flex flex-col items-center justify-center text-4xl font-medium z-50">
+                    <div className="py-4 slide delay cursor-pointer" onClick={toggleMobileMenu}>
+                        Projects
+                    </div>
+                    <div className="py-4 slide delay cursor-pointer" onClick={toggleMobileMenu}>
+                        About
+                    </div>
+                    <div className="py-4 slide delay cursor-pointer" onClick={toggleMobileMenu}>
+                        Resume
+                    </div>
+                </div>
+            )}
+        </div>
         </div>
     );
 };
--- Components/hero.tsx
+++ Components/hero.tsx
@@ -1,32 +1,66 @@
 import React from "react";
 import "../app/globals.css";
-import { Button, ChakraProvider, Stack } from '@chakra-ui/react';
-import { MdEmail } from "react-icons/md";
-import { FaLinkedin } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
+import { Button, ChakraProvider, Stack } from "@chakra-ui/react";
+import { FaLinkedin, FaGithub, FaEnvelope } from "react-icons/fa";
 
 const Hero: React.FC = () => {
     return (
         <ChakraProvider>
-            <>
-                <div className="max-w-screen-xl w-full my-0 mx-auto px-0 pt-[24vh] pb-[16vh]">
-                    <h1 className="text-7xl font-poppins font-semibold heroText pb-8 slide delay01">Software Developer.
-                        <br />Crafting efficient solutions with a love for Computer Science.</h1>
-                    <div className="pt-8">
-                        <Stack direction='row' spacing={4}>
-                            <Button leftIcon={<FaGithub />} colorScheme='teal' variant='solid'>
-                                Github
-                            </Button>
-                            <Button leftIcon={<FaLinkedin />} colorScheme='teal' variant='solid'>
-                                LinkedIn
-                            </Button>
-                            <Button leftIcon={<MdEmail />} colorScheme='teal' variant='solid'>
-                                Email
-                            </Button>
-                        </Stack>
-                    </div>
+            <div className="max-w-screen-xl w-full my-0 mx-auto px-4 pt-[16vh] pb-[12vh] md:pt-[24vh] md:pb-[16vh]">
+                <h1 className="text-3xl md:text-6xl lg:text-7xl font-poppins font-semibold heroText pb-6 md:pb-8 slide delay01">
+                    Software Developer.
+                    <br />
+                    Crafting solutions with coding.
+                </h1>
+                <div>
+                    <Stack
+                    direction="row"
+                    spacing={4}
+                    align="center"
+                    justify={{ sm: "space-evenly", md: "flex-start" }}
+                >
+                    <a
+                        href="https://github.com"
+                    >
+                        <Button
+                            leftIcon={<FaGithub />}
+                            colorScheme="black"
+                            variant="outline"
+                            size={{ base: "sm", md: "md" }}
+                            className="flex items-center px-6 py-3 text-lg font-medium hover:bg-[#f5f5f5] transition duration-300 ease-in-out"
+                        >
+                            Github
+                        </Button>
+                    </a>
+                    <a
+                        href="https://linkedin.com"
+                    >
+                        <Button
+                            leftIcon={<FaLinkedin />}
+                            colorScheme="black"
+                            variant="outline"
+                            size={{ base: "sm", md: "md" }}
+                            className="flex items-center px-6 py-3 text-lg font-medium hover:bg-[#f5f5f5] transition duration-300 ease-in-out"
+                        >
+                            LinkedIn
+                        </Button>
+                    </a>
+                    <a
+                        href="mailto:example@example.com"
+                    >
+                        <Button
+                            leftIcon={<FaEnvelope />}
+                            colorScheme="black"
+                            variant="outline"
+                            size={{ base: "sm", md: "md" }}
+                            className="flex items-center px-6 py-3 text-lg font-medium hover:bg-[#f5f5f5] transition duration-300 ease-in-out"
+                        >
+                            Email
+                        </Button>
+                    </a>
+                </Stack>
                 </div>
-            </>
+            </div>
         </ChakraProvider>
     );
 };
