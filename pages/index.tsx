22f48d1a61e294b17178770a46d3b754fdd3942f
marmik28 <marmik28112002@gmail.com>
2024-08-30 14:59:05 -0400

    Added the Resume section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Hero from "@/Components/hero";
 import Header from "@/Components/header";
-import AboutMe from "@/Components/aboutme";
+import AboutMe from "@/Components/about";
 import ProjectsSection from "@/Components/projects";
-
+import ResumeSection from "@/Components/resume";
 
 const Home = () => {
   return (
@@ -12,6 +12,7 @@ const Home = () => {
       <Hero />
       <AboutMe />
       <ProjectsSection />
+      <ResumeSection />
     </>
   );
 };
